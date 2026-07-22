39793a61920b7208bd116aef8a9a41ed3cf81eb5
ntngoeec025 <ntngoeec025@student.wethinkcode.co.za>
2026-07-22 12:07:56 +0200

    Library Catalog

--- src/main/java/za/co/wethinkcode/service/LibraryCatalog.java
+++ src/main/java/za/co/wethinkcode/service/LibraryCatalog.java
@@ -6,31 +6,51 @@ import java.util.*;
 public class LibraryCatalog {
     private final String libraryName;
     // TODO: Store PrintMedia items in a list
+    private List<PrintMedia> printMedia;
+    private List<DigitalMedia> digitalMedia;
     // TODO: Store DigitalMedia items in a list
 
     public LibraryCatalog(String libraryName) {
         this.libraryName = libraryName;
+        this.printMedia = new ArrayList<>();
+        this.digitalMedia = new ArrayList<>();
     }
 
     public String getLibraryName() { return libraryName; }
 
     // TODO: Register PrintMedia
     public void addPrintMedia(PrintMedia item) {
+        if (item != null){
+            this.printMedia.add(item);
+        }
     }
 
     // TODO: Register DigitalMedia
     public void addDigitalMedia(DigitalMedia item) {
+        if (item != null){
+            this.digitalMedia.add(item);
+        }
     }
 
     // TODO: Calculate potential late fee for all PrintMedia items for a given number of days overdue.
     // RULE: Only calculate fees for items where isBorrowable() == true!
     public double calculateTotalLateFees(int daysOverdue) {
-        return 0.0;
+        double amount = 0.0;
+        for (PrintMedia item : printMedia){
+            if (item.isBorrowable()){
+                amount += item.calculateLateFee(daysOverdue);
+            }
+        }
+        return amount;
     }
 
     // TODO: Calculate total streaming cost for all DigitalMedia items cataloged.
     public double calculateTotalDigitalStreamCost() {
-        return 0.0;
+        double amount =0.0;
+        for (DigitalMedia item : digitalMedia){
+            amount += item.getStreamCost();
+        }
+        return amount;
     }
 
     // TODO: Search for a PrintMedia item by title. Return null if not found.
